cdac25f6916ef36fce189f44a10fba4be27bf603
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-10-21 01:31:36 +0300

    Loader

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,15 +1,6 @@
 const Loader = () => {
     return (
-         <h1 className="title">
-            <span className="letter let1">L</span>  
-            <span className="letter let2">o</span>  
-            <span className="letter let3">a</span>  
-            <span className="letter let4">d</span>  
-            <span className="letter let5">i</span>  
-            <span className="letter let6">n</span>  
-            <span className="letter let7">g</span>  
-        </h1>
-
+        <div className="loader"></div>
     )
 }
 
