13c2f8f9508ff7aecf375de7e1d42c489f1e9217
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-10-20 15:11:50 +0300

    Add search Icon

--- client/src/components/SearchForm/SearchForm.tsx
+++ client/src/components/SearchForm/SearchForm.tsx
@@ -1,10 +1,21 @@
 import { Button, FormControl, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
 // HARDCODE
 const SearchForm = ({ search, city }: { search: string | undefined, city: string | undefined }) => {
     return (
         <form action={'/search'}>
             <Stack direction={{ md: "row" }} gap={{ xs: 2, md: 1 }} justifyContent={"center"} sx={{ p: 4, bgcolor: "bgcolor.main", borderRadius: 1, maxWidth: "48rem", mx: "auto" }}>
-                <TextField sx={{ flex: 1 }} name="query" defaultValue={search || ""} placeholder="Въведете име на продукт (например: мляко, хляб, чай)" />
+                <TextField
+                    sx={{ flex: 1 }}
+                    name="query"
+                    defaultValue={search || ""}
+                    placeholder="Въведете име на продукт (например: мляко, хляб, чай)"
+                    slotProps={{
+                        input: {
+                            startAdornment: <SearchIcon sx={{ mr: 1 }} />,
+                        },
+                    }}
+                />
                 <FormControl sx={{ width: { md: "10rem" } }}>
                     <InputLabel>Град</InputLabel>
                     <Select name="city" defaultValue={city || ""}>
