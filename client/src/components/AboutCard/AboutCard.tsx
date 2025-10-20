c1861d7d0a940895dfdf6f5768c9e72ee61e2c0d
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-10-20 15:27:00 +0300

    About Card animation

--- client/src/components/AboutCard/AboutCard.tsx
+++ client/src/components/AboutCard/AboutCard.tsx
@@ -4,7 +4,7 @@ import { FC } from "react"
 const AboutCard = ({ title, text, Icon }: { title: string, text: string, Icon: FC<SvgIconProps> }) => {
     return (
         <Grid size={{ xs: 12, md: 4 }}>
-            <Card sx={{ p: 4, height: "100%", width: "100%" }} component={Stack} alignItems={"center"}>
+            <Card sx={{ p: 4, height: "100%", width: "100%", transition: ".2s", "&:hover": { transform: "translateY(-0.5rem)" } }} component={Stack} alignItems={"center"}>
                 <Stack justifyContent={"center"} alignItems={"center"} mb={2} sx={{
                     p: 2,
                     bgcolor: "primary.light",
