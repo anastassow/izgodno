11a0691e4436d9aba95aa9e0a15ec1d0160a28e6
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-10-21 00:29:21 +0300

    Loader

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,6 +1,25 @@
+import Loader from "@/components/Loader/Loader"
+import SectionWrapper from "@/components/SectionWrapper/SectionWrapper"
+import { Stack } from "@mui/material"
+
 const Loading = () => {
     return (
-        <>Loading...</>
+        <SectionWrapper props={{
+            sx: {
+                minHeight: "100vh"
+            }
+        }}>
+            <Stack
+                sx={{
+                    width: "100%",
+                    minHeight: "100vh"
+                }}
+                justifyContent={"center"}
+                alignItems={"center"}
+            >
+                <Loader />
+            </Stack>
+        </SectionWrapper>
     )
 }
 
