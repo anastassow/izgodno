1a26ac645d21efd90befeb3216499c336a6382dd
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-10-21 02:02:53 +0300

    Clean Up

--- client/src/components/SearchPagination/SearchPagination.tsx
+++ client/src/components/SearchPagination/SearchPagination.tsx
@@ -1,31 +1,22 @@
 'use client'
 
 import { Pagination, PaginationItem } from "@mui/material"
-import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 
 const SearchPagination = ({ count, page }: { count: number, page: number }) => {
-    // const router = useRouter();
     const searchParams = useSearchParams()
     const pathName = usePathname()
-
-    const handleChange = (event: React.ChangeEvent<unknown>, page: number) => {
-        // Redirect to the same route but with updated `page` query param
-        // console.log(router.getQuery);
-        console.log(page)
-    };
 
     return (
         <>
             <Pagination
-                onChange={handleChange}
                 page={page + 1}
                 size="large"
                 sx={{ mx: "auto", display: count <= 1 ? "none" : "block" }}
-                count={count} 
+                count={20} 
                 color="primary"
                 renderItem={(item) => (
-                    <a href={`${pathName}?query=${searchParams.get("query")}&city=${searchParams.get("city")}&page=${item.page || 0}`}>
+                    <a href={`${pathName}?query=${searchParams.get("query")}&city=${searchParams.get("city")}&page=${(item.page || 1) - 1}`}>
                         <PaginationItem
                             {...item}
                         />
