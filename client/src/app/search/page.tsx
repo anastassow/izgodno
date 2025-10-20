45786c66bcb96ef4416be76ffb81e8d338c39f8b
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-10-20 15:54:27 +0300

    Add metadata

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -6,9 +6,52 @@ import StoreCard from "@/components/StoreCard/StoreCard";
 import Image from "next/image";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Link from "next/link";
+import { Metadata, ResolvingMetadata } from "next";
+
+type SearchParams = {
+    searchParams: { [key: string]: string | undefined }
+}
+
+export async function generateMetadata(
+  { searchParams }: SearchParams,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { query, city } = await searchParams
+ 
+  return {
+    title: `${query} - продукти в ${city}`,
+    description: "Разгледай резултатите от търсенето и сравни цени на хранителни продукти от различни супермаркети. Използвай интелигентно търсене с изкуствен интелект и филтрирай лесно по цена, категория или марка.",
+    openGraph: {
+        title: "Резултати от търсене – Izgodno",
+        description: "Сравнявай хранителни продукти от различни супермаркети на едно място. Използвай AI филтри и ежедневно обновявани данни за най-точни резултати.",
+        // url: "https://comparegrocer.com/search",
+        siteName: "Izgodno",
+        // images: [
+        //     {
+        //         url: "/og-image.jpg",
+        //         width: 1200,
+        //         height: 630,
+        //         alt: "Резултати от търсене в CompareGrocer",
+        //     },
+        // ],
+        locale: "bg_BG",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Izgodno – Резултати от търсене",
+        description: "Открий и сравни цени на хранителни продукти от различни супермаркети. Интелигентно търсене, ежедневно обновяване и най-добрите оферти.",
+        // images: ["/og-image.jpg"],
+    },
+    robots: {
+        index: true,
+        follow: true,
+    },
+  }
+}
 
 // HARDCODE
-const Page = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+const Page = async ({ searchParams }: SearchParams) => {
     const { query, city } = await searchParams
     
     const products = 9
